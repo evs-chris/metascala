a92bc82f54a3148f026c1509ccaaab29b079090c
Chris Reeves <chris@ev-soft.net>
2011-10-23 02:29:12 -0400

    From metascala, changed HCons to |: and renamed all methods to match.

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "Playground"
+
+version := "1"
+
+scalaVersion := "2.9.1"
---
+++ src/main/scala/metascala/test/UnitsTest.scala
@@ -0,0 +1,9 @@
+package metascala.test
+
+object UnitsTest {
+  import metascala.Units._
+  
+  val dist : Length = m(2.3)
+  val time : Time = s(1.7)
+  //val speed : Speed = dist / time
+}
